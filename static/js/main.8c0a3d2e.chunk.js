1a2f5b7344c5ff18f66f735b79cb7ab6c5c4c745
Oleg <kapustianskyov@gmail.com>
2020-11-23 20:36:53 +0200

    Updates

---
+++ static/js/main.8c0a3d2e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpiconfont=this.webpackJsonpiconfont||[]).push([[0],{10:function(n,t,e){},4:function(n,t,e){"use strict";e.r(t);var c=e(2),o=(e(1),e(3)),s=e.n(o),i=(e(10),function(){return Object(c.jsx)("section",{className:""})});s.a.render(Object(c.jsx)(i,{}),document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.8c0a3d2e.chunk.js.map
